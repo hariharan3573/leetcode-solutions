a5f2b8bfc59127417c42c26772862a325dc8ed70
HARIHARAN <haraiharan3112004@gmail.com>
2026-07-12 13:03:31 +0530

    2798. Number of Employees Who Met the Target

--- easy.js
+++ easy.js
@@ -37,18 +37,31 @@
 // 
 
 
-1431. Kids With the Greatest Number of Candies
+// 1431. Kids With the Greatest Number of Candies
 
 
-var kidsWithCandies = function(candies, extraCandies) {
-    let arr=[];
-    let max=Math.max(...candies);
-    for(let i=0;i<candies.length;i++){
-        if(candies[i]+extraCandies>=max){
-            arr.push(true);
-        }else{
-            arr.push(false)
+// var kidsWithCandies = function(candies, extraCandies) {
+//     let arr=[];
+//     let max=Math.max(...candies);
+//     for(let i=0;i<candies.length;i++){
+//         if(candies[i]+extraCandies>=max){
+//             arr.push(true);
+//         }else{
+//             arr.push(false)
+//         }
+//     }
+//     return arr
+// };
+
+2798. Number of Employees Who Met the Target
+
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count=0;
+    for(let i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+            count++
         }
     }
-    return arr
+    return count
 };
+
